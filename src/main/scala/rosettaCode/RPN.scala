deac96ef1203408649c39f49936f87009eced76c
Cary Scofield <carys689@gmail.com>
2015-05-21 10:48:56 -0400

    Remove direct dependencies on Java libraries; some refactoring

--- src/main/scala/rosettaCode/RPN.scala
+++ src/main/scala/rosettaCode/RPN.scala
@@ -1,7 +1,5 @@
 package rosettaCode
 
-import java.util.StringTokenizer
-import scala.collection.mutable.Stack
 
 /**
  * Solution for http://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm
@@ -9,7 +7,7 @@ import scala.collection.mutable.Stack
  * If the program runs correctly, there should be no output (i.e., all assertions succeed)
  *
  * @author Cary Scofield (carys689 <at> gmail <dot> com) on 5/21/2015.
- * @since Scala 2.11.2; Java 1.7
+ * @since Scala 2.11.2
  */
 object RPN {
   def main(args: Array[String]): Unit = {
@@ -20,9 +18,9 @@ object RPN {
       assert(solve("5 3 -") == -2, "5 3 - != -2")
       assert(solve("3 2 ^") == 8, "3 2 ^ != 8")
       assert(farenheitToCelsius(32) == 0, "farentheitToCelsius(32) != 0")
-      assert(equal(farenheitToCelsius(0), -17.7778), "farenheitToCelsius(0) != -17.7778")
+      assert(isEqual(farenheitToCelsius(0), -17.7778), "farenheitToCelsius(0) != -17.7778")
       assert(celsiusToFarenheit(0) == 32, "celsiusToFarenheit(0) != 32")
-      assert(equal(celsiusToFarenheit(-32), -25.6), "celsiusToFarenheit(-32) != -25.6")
+      assert(isEqual(celsiusToFarenheit(-32), -25.6), "celsiusToFarenheit(-32) != -25.6")
     }
     catch {
       case e: AssertionError => println(e.toString)
@@ -30,13 +28,14 @@ object RPN {
     }
   }
 
-  def equal(actual: Double, expected: Double): Boolean = {
+  // Test if two floating-point numbers are 'equal'
+  def isEqual(actual: Double, expected: Double): Boolean = {
     val EPSILON: Double = 0.0001
-    java.lang.Math.abs(actual - expected) < EPSILON
+    math.abs(actual - expected) < EPSILON
   }
 
   def solve(equation: String): Double = {
-    evaluate(new StringTokenizer(equation, " "))
+    evaluate(equation.split(" ").toList)
   }
 
   def farenheitToCelsius(ftemp: Double): Double = {
@@ -47,25 +46,21 @@ object RPN {
     solve(s"$ctemp 5 9 / * 32 +")
   }
 
-  def evaluate(st: StringTokenizer): Double = {
+  def evaluate(tokens: List[String]): Double = {
+    import scala.collection.mutable.Stack
     val stack: Stack[Double] = new Stack[Double]
-    while (st.hasMoreTokens) {
-      val token = st.nextToken.trim
-      if (isOperator(token)) evaluate(token, stack)
-      else stack.push(java.lang.Double.parseDouble(token))
+    for( token <- tokens ) {
+      if (isOperator(token)) token match {
+        case "+" => stack.push(stack.pop + stack.pop)
+        case "-" => stack.push(stack.pop - stack.pop)
+        case "*" => stack.push(stack.pop * stack.pop)
+        case "/" => stack.push(stack.pop / stack.pop)
+        case "^" => stack.push(math.pow(stack.pop, stack.pop))
+        case _ => throw new RuntimeException( s""""$token" is not an operator""")
+      }
+      else stack.push(token.toDouble)
     }
     stack.pop
-  }
-
-  def evaluate(operator: String, stack: Stack[Double]): Unit = {
-    operator match {
-      case "+" => stack.push(stack.pop + stack.pop)
-      case "-" => stack.push(stack.pop - stack.pop)
-      case "*" => stack.push(stack.pop * stack.pop)
-      case "/" => stack.push(stack.pop / stack.pop)
-      case "^" => stack.push(java.lang.Math.pow(stack.pop, stack.pop))
-      case _   => throw new RuntimeException(s""""$operator" is not an operator""")
-    }
   }
 
   def isOperator(token: String): Boolean = {
