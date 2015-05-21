9531ce2588d01b2f2570bd99ca481c8af593e640
Cary Scofield <carys689@gmail.com>
2015-05-21 12:38:48 -0400

    Reverse operands for -, /, and ^ operators

--- src/main/scala/rosettaCode/RPN.scala
+++ src/main/scala/rosettaCode/RPN.scala
@@ -4,19 +4,24 @@ package rosettaCode
 /**
  * Solution for http://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm
  *
- * If the program runs correctly, there should be no output (i.e., all assertions succeed)
+ * If the program runs correctly, there should be no assertion failures.
  *
  * @author Cary Scofield (carys689 <at> gmail <dot> com) on 5/21/2015.
  * @since Scala 2.11.2
  */
 object RPN {
+  val PRINT_STACK_CONTENTS: Boolean = false
+
   def main(args: Array[String]): Unit = {
     try {
+      val result = solve("3 4 2 * 1 5 - 2 3 ^ ^ / +")
+      println(result)
+      assert(isEqual(result, 3.00012))
       assert(solve("3 5 +") == 8, "3 5 + != 8")
       assert(solve("3 5 *") == 15, "3 5 * != 15")
-      assert(solve("3 6 /") == 2, "3 6 / != 2")
-      assert(solve("5 3 -") == -2, "5 3 - != -2")
-      assert(solve("3 2 ^") == 8, "3 2 ^ != 8")
+      assert(solve("6 3 /") == 2, "3 6 / != 2")
+      assert(solve("3 5 -") == -2, "5 3 - != -2")
+      assert(solve("2 3 ^") == 8, "3 2 ^ != 8")
       assert(farenheitToCelsius(32) == 0, "farentheitToCelsius(32) != 0")
       assert(isEqual(farenheitToCelsius(0), -17.7778), "farenheitToCelsius(0) != -17.7778")
       assert(celsiusToFarenheit(0) == 32, "celsiusToFarenheit(0) != 32")
@@ -31,7 +36,7 @@ object RPN {
   // Test if two floating-point numbers are 'equal'
   def isEqual(actual: Double, expected: Double): Boolean = {
     val EPSILON: Double = 0.0001
-    math.abs(actual - expected) < EPSILON
+    math.abs(actual) - math.abs(expected) < EPSILON
   }
 
   def solve(equation: String): Double = {
@@ -39,27 +44,36 @@ object RPN {
   }
 
   def farenheitToCelsius(ftemp: Double): Double = {
-    solve(s"9 5 / 32 $ftemp - *")
+    var result = solve(s"5 9 / 32 $ftemp - *")
+    result
   }
 
   def celsiusToFarenheit(ctemp: Double): Double = {
-    solve(s"$ctemp 5 9 / * 32 +")
+    solve(s"$ctemp 9 5 / * 32 +")
   }
 
   def evaluate(tokens: List[String]): Double = {
     import scala.collection.mutable.Stack
     val stack: Stack[Double] = new Stack[Double]
-    for( token <- tokens ) {
+    for (token <- tokens) {
       if (isOperator(token)) token match {
         case "+" => stack.push(stack.pop + stack.pop)
-        case "-" => stack.push(stack.pop - stack.pop)
+        case "-" => val x = stack.pop; stack.push(stack.pop - x)
         case "*" => stack.push(stack.pop * stack.pop)
-        case "/" => stack.push(stack.pop / stack.pop)
-        case "^" => stack.push(math.pow(stack.pop, stack.pop))
+        case "/" => val x = stack.pop; stack.push(stack.pop / x)
+        case "^" => val x = stack.pop; stack.push(math.pow(stack.pop, x))
         case _ => throw new RuntimeException( s""""$token" is not an operator""")
       }
       else stack.push(token.toDouble)
+
+      if (PRINT_STACK_CONTENTS) {
+        print("Input: " + token)
+        print(" Stack: ")
+        for (element <- stack.seq.reverse) print(element + " ");
+        println("")
+      }
     }
+
     stack.pop
   }
 
